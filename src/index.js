7b2aae14975fdd8646397c259b1d0553856acb92
Nikita Belous <belousnikita@gmail.com>
2019-03-01 20:48:36 +0200

    Colorpicker added

--- src/index.js
+++ src/index.js
@@ -2,32 +2,76 @@ import Background from 'bg-canvases';
 import Circle from './Circle';
 import Vector from './Vector';
 import classnames from 'classnames';
+import Rainbow from 'rainbowvis.js';
+import Vue from 'vue';
+import uniqueId from 'lodash/uniqueId';
 
 const bg = new Background();
 
+const colorPalette = new Rainbow();
+
+var colorlistA = new Vue({
+    el: '#colorpicker',
+    data: {
+        items: [
+            { colorcell: { color: '#81F4E1', id: uniqueId() } },
+            { colorcell: { color: '#5EC2B7', id: uniqueId() } },
+            { colorcell: { color: '#F4ACB7', id: uniqueId() } },
+            { colorcell: { color: '#339999', id: uniqueId() } },
+            { colorcell: { color: '#81F4E1', id: uniqueId() } },
+        ]
+    },
+    methods: {
+        add: () => {
+            const swatch = document.getElementById('color');
+            const color = swatch.value;
+            if (colorlistA.items.length < 20)
+                colorlistA.items = [...colorlistA.items, { colorcell: { color } }];
+            const colors = colorlistA.$data.items.map(item => item.colorcell.color);
+            colorPalette.setSpectrum(...colors);
+            bg.applyOnEach(c => c.setColorPalette(colorPalette));
+        },
+        remove: (i) => {
+            if (colorlistA.items.length > 2) {
+                colorlistA.items.splice(i, 1);
+                const colors = colorlistA.$data.items.map(item => item.colorcell.color);
+                colorPalette.setSpectrum(...colors);
+                bg.applyOnEach(c => c.setColorPalette(colorPalette));
+            } else if (colorlistA.items.length === 2) {
+                colorlistA.items.splice(i, 1);
+                const colors = colorlistA.$data.items.map(item => item.colorcell.color);
+                colorPalette.setSpectrum(...colors, ...colors);
+                bg.applyOnEach(c => c.setColorPalette(colorPalette));
+            }
+        },
+    }
+});
+const colors = colorlistA.$data.items.map(item => item.colorcell.color);
+colorPalette.setSpectrum(...colors);
 const canvas = document.getElementById('canvas');
 const flow = document.getElementById('flow');
 const swap = document.getElementById('swap');
 const bubble = document.getElementById('bubble');
-
-const touchpad = document.getElementById('touchpad');
 const ctx = canvas.getContext('2d');
-const scaleDpi = (...contexts) => {
-    const dpr = window.devicePixelRatio || 1,
-        scale = ctx => {
-            const rect = ctx.canvas.getBoundingClientRect();
-            ctx.canvas.width = rect.width * dpr;
-            ctx.canvas.height = rect.height * dpr;
-            ctx.scale(dpr, dpr);
-        };
-    contexts.forEach(c => scale(c));
+const resizeCanvas = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    cfg.centerX = window.innerWidth / 2;
+    cfg.centerY = window.innerHeight / 2;
+    bg.applyOnEach((c => {
+        const newInitialTarget = Circle.getPositionInCircle(
+            parseInt(c.i, 10), cfg.centerX, cfg.centerY, cfg.circleRadius, cfg.quantity);
+        c.setInitialTarget(newInitialTarget);
+    }));
+    ctx.globalAlpha = cfg.alpha;
 };
+
 const cfg = {
     quantity: 150,
     centerX: window.innerWidth / 2,
     centerY: window.innerHeight / 2,
     circleRadius: 150,
-    alpha: 0.8,
+    alpha: 1,
 };
 const state = {
     mouseX: 0,
@@ -35,9 +79,11 @@ const state = {
     active: false,
 };
 
-scaleDpi(ctx);
+
+colorPalette.setNumberRange(0, cfg.quantity);
+resizeCanvas();
 ctx.globalAlpha = cfg.alpha;
-const creator = Circle.makeRingCreator(cfg.centerX, cfg.centerY, cfg.circleRadius, cfg.quantity);
+const creator = Circle.makeRingCreator(cfg.centerX, cfg.centerY, cfg.circleRadius, cfg.quantity, colorPalette);
 
 cfg.bubble = (c) => {
 
@@ -59,35 +105,19 @@ cfg.bubble = (c) => {
     else {
         c.setRadius(c.initialRadius + radiusPlus);
         c.resetTarget();
-        // c.resetSpeed();
     }
     c.lookFor().update();
 };
 
 cfg.swap = (c) => {
-
     const oppositePos = Circle.getOppostitePos(c.initialTarget, cfg.centerX, cfg.centerY);
-    // const ditstToOpposite = c.pos.distanceTo(oppositePos);
-
     c.setSpeed(1 * Math.random() + 1);
     if (state.active) {
         c.setTarget(oppositePos);
-        // if (ditstToOpposite <= c.radius * 2) {
-        //     c.setPos(oppositePos);
-        //     c.resetAcc();
-        //     c.resetSpeed();
-        // }
     }
     else {
         c.resetTarget();
         c.resetAcc();
-        // const distToInitial = c.pos.distanceTo(c.initialTarget);
-        // if (distToInitial <= c.radius * 2) {
-        //     c.resetPos();
-        //     c.resetAcc();
-        //     c.resetSpeed();
-        //     c.resetTarget();
-        // }
     }
     c.lookFor().update();
 };
@@ -129,26 +159,16 @@ const getMousePos = (canvas, evt) => {
         y: evt.clientY - rect.top
     };
 };
-touchpad.addEventListener('mousemove', (e) => {
+window.addEventListener('mousemove', (e) => {
     const pos = getMousePos(canvas, e);
     state.mouseX = pos.x;
     state.mouseY = pos.y;
     state.active = true;
 });
-touchpad.addEventListener('mouseleave', () => { state.active = false; });
+canvas.addEventListener('mouseleave', () => { state.active = false; });
 
-window.onresize = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    cfg.centerX = window.innerWidth / 2;
-    cfg.centerY = window.innerHeight / 2;
-    bg.applyOnEach((c => {
-        const newInitialTarget = Circle.getPositionInCircle(
-            parseInt(c.i, 10), cfg.centerX, cfg.centerY, cfg.circleRadius, cfg.quantity);
-        c.setInitialTarget(newInitialTarget);
-    }));
-    ctx.globalAlpha = cfg.alpha;
-};
+
+window.onresize = resizeCanvas;
 const changeFunction = (e) => {
     bg.applyOnEach(c => {
         c.setSpeed(0.5);
@@ -163,9 +183,35 @@ const changeFunction = (e) => {
     };
     flow.classList = getClass('flow');
     bubble.classList = getClass('bubble');
-    swap.classList = getClass('spap');
+    swap.classList = getClass('swap');
 
 };
 flow.onclick = changeFunction;
 bubble.onclick = changeFunction;
 swap.onclick = changeFunction;
+
+const alphaInput = document.querySelector('input[name=alpha]');
+alphaInput.addEventListener('input', (e) => {
+    const alpha = e.target.value;
+    cfg.alpha = alpha;
+    ctx.globalAlpha = alpha;
+});
+
+// colorpicker.onmousedown = () => {
+
+//     const className = classnames('colorpicker', { active: true });
+//     const listClassName = classnames('colorlist', { active: true });
+//     colorpicker.classList = className;
+//     colorlist.classList = listClassName;
+
+//     clearTimeout(timer);
+
+// };
+// colorpicker.onmouseleave = () => {
+//     timer = setTimeout(function () {
+//         const className = classnames('colorpicker', { active: false });
+//         const listClassName = classnames('colorlist', { active: false });
+//         colorpicker.classList = className;
+//         colorlist.classList = listClassName;
+//     }, delay);
+// };
